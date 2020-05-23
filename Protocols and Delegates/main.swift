a573d0bc617396448ded8ed3f2b65978588c4320
Naohisa Yokoyama <nhsykym@gmail.com>
2020-05-23 15:25:22 +0900

    Initial Commit

---
+++ Protocols and Delegates/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Protocols and Delegates
+//
+//  Created by 横山尚久 on 2020/05/23.
+//  Copyright © 2020 横山尚久. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
